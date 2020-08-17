ab077e376bf70032b13bdf1721e8851269f61062
mvts1 <omavitas@gmail.com>
2020-08-17 15:27:29 +0300

    Initial Commit

---
+++ YourStylist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourStylist
+//
+//  Created by Onur Mavitas on 17.08.2020.
+//  Copyright © 2020 Onur Mavitas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
